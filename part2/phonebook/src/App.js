ee77005d766eac029823c5df348636dbaa587573
beyond-boreas <114255759+beyond-boreas@users.noreply.github.com>
2023-02-06 20:05:06 -0800

    Complete exercise 2.8, 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,9 +3,15 @@ import { useState } from 'react'
 const App = () => {
   // STATE VARIABLES //
   const [persons, setPersons] = useState([
-    { name: 'Sasha Conger' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
   ])
+  const [showPersons, setShowPersons] = useState(persons)
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
 
 
   // FUNCTIONS //
@@ -15,24 +21,50 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  const handleSearchChange = (event) => {
+    const currentSearch = event.target.value
+    console.log(event.target.value)
+    setSearch(currentSearch)
+
+    // Persons to show, ignoring search case
+    const newShowPersons = persons.filter(person => person.name.toLowerCase().includes(currentSearch.toLowerCase()) === true)
+    
+    console.log(newShowPersons)
+    setShowPersons(newShowPersons)
+
+    
+  }
   
   // When the 'add' button is clicked, add a new name to the 'persons' state object
   const handleAddName = (event) => {
     event.preventDefault()
 
-    const personsObject = {
-      name: newName
+    const personObject = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1
     }
 
     // console.log(persons.some(e => e.name === personsObject.name))
     // Check if the person being added already exists
-    if (persons.some(e => e.name === personsObject.name)) {
-      alert(`${personsObject.name} already exists!`)
+    if (persons.some(e => e.name === personObject.name)) {
+      alert(`${personObject.name} already exists!`)
+      return
+    }
+    else if (persons.some(e => e.number === personObject.number)) {
+      alert(`The number ${personObject.number} already exists!`)
       return
     }
 
-    setPersons(persons.concat(personsObject))
-    // console.log(persons)
+    setPersons(persons.concat(personObject))
+    setShowPersons(persons.concat(personObject))
+    setSearch('')
+    console.log(persons)
   }
 
 
@@ -41,31 +73,38 @@ const App = () => {
     return (
       <ul>
         {persons.map(person =>
-          <li key={person.name}>
-            {person.name}
+          <li key={person.id}>
+            {person.name} {person.number}
           </li>
         )}
       </ul>
     )
   }
 
-  // const Number = ({persons}) => {
-  //   return (
-  //     <>
-  //       {persons.map(person )}
-  //     </>
-  //   )
-  // }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: 
-          <input 
+          filter shown with <input 
+            value={search}
+            onChange={handleSearchChange}
+          />
+        </div>
+      </form>
+      <h2>Add a New Entry</h2>
+      <form>
+        <div>
+          name: <input 
             value={newName}
             onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          number: <input 
+            value={newNumber}
+            onChange={handleNumberChange}
           />
         </div>
         <div>
@@ -73,7 +112,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Numbers persons={persons}/>
+      <Numbers persons={showPersons}/>
     </div>
   )
 }
