94bc3d34f0c802ab6b350f77ef5d9e127832db80
beyond-boreas <114255759+beyond-boreas@users.noreply.github.com>
2023-02-07 20:28:21 -0800

    Complete exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter.js'
 import PersonForm from './components/PersonForm.js'
 import Numbers from './components/Numbers.js'
 
+import axios from 'axios'
+
 const App = () => {
   // STATE VARIABLES //
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ])
+  // const [persons, setPersons] = useState([
+  //   { name: 'Arto Hellas', number: '040-123456', id: 1 },
+  //   { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+  //   { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+  //   { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  // ])
+  const [persons, setPersons] = useState([])
   const [showPersons, setShowPersons] = useState(persons)
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
+
+  // EFFECT
+  const hook = () => {
+    console.log('effect run')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+        setShowPersons(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
 
 
   // EVENT HANDLERS //
