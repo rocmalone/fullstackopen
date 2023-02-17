278ee1a82545e1e3ea99cedb7ed684c59a7ca0a6
beyond-boreas <114255759+beyond-boreas@users.noreply.github.com>
2023-02-16 20:57:27 -0800

    Write notes to backend, add toggle importance

--- part2/scratchwork/src/App.js
+++ part2/scratchwork/src/App.js
@@ -8,19 +8,43 @@ const App = (props) => {
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
+
+  // FUNC:  TOGGLE IMPORTANCE OF A NOTE.
+  // Passed to the <Note> component.
+  const toggleImportanceOf = (id) => {
+    // Define the unique URL of each note via its id.
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    // Create a new object from the original note and overwrite the 
+    // 'important' property by assigning it again.
+    const changedNote = { ...note, important: !note.important }
+
+    // HTTP PUT replaces an entire note with a new note.
+    axios
+      .put(url, changedNote)
+      .then(response => {
+        // Create a new array by mapping every item from the old array into an item in the new array. If note.id !== id is true, we copy the item from the old array into the new array. If false, then the note object returned by the server is added to the array instead.
+        setNotes(notes.map(n => n.id !== id ? n : response.data))
+      })
+    console.debug(`Importance of ${id} is toggled`)
+  }
+
+
+  // FUNC:  LOAD INITIAL NOTES FROM db.json FILE ON SERVER
+  // The hook is executed as an effect with useEffect
   const hook = () => {
-    console.log('effect')
+    // HTTP GET NOTES FROM db.json
     axios
       .get('http://localhost:3001/notes')
       .then(response => {
-        console.log('promise fulfilled')
+        console.debug("Axios response from loading initial notes:", response)
         setNotes(response.data)
       })
   }
-  
-  useEffect(hook, [])
+  useEffect(hook, []) // Effects occur once at the end of rendering
 
-  console.log('render', notes.length, 'notes')
+
+  // FUNC:  ADD NOTE ON CLICK 'save' BUTTON
   const addNote = (event) => {
     event.preventDefault()
 
@@ -30,20 +54,27 @@ const App = (props) => {
       id: notes.length + 1,
     }
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
-
-    // console.log('button clicked', event.target)
+    // HTTP POST NEW NOTE INTO db.json
+    axios
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        console.debug("Axios response from adding note:", response)
+        setNotes(notes.concat(noteObject))
+        setNewNote('')
+      })
   }
+
 
   const handleNoteChange = (event) => {
     console.log(event.target.value)
     setNewNote(event.target.value)
   }
 
+
   const notesToShow = showAll
     ? notes
     : notes.filter(note => note.important === true)
+
 
   return (
     <div>
@@ -55,7 +86,12 @@ const App = (props) => {
       </div>
       <ul>
         {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
+          <Note 
+            key={note.id} 
+            note={note} 
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          
+          />
         )}
       </ul>
       <form onSubmit={addNote}> 
