f3489d025bea57a11d22544dc23bb16f0a1b1ff5
beyond-boreas <114255759+beyond-boreas@users.noreply.github.com>
2023-02-20 20:50:03 -0800

    Progress through part 2

--- part2/scratchwork/src/App.js
+++ part2/scratchwork/src/App.js
@@ -2,6 +2,8 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
+// Import backend note handling
+import noteService from './services/notes'
 
 const App = (props) => {
   const [notes, setNotes] = useState([])
@@ -9,39 +11,32 @@ const App = (props) => {
   const [showAll, setShowAll] = useState(true)
 
 
+  // LOAD INITIAL NOTES FROM db.json FILE ON SERVER
+  // Effects occur once at the end of initial rendering
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
+
+
   // FUNC:  TOGGLE IMPORTANCE OF A NOTE.
   // Passed to the <Note> component.
   const toggleImportanceOf = (id) => {
-    // Define the unique URL of each note via its id.
-    const url = `http://localhost:3001/notes/${id}`
     const note = notes.find(n => n.id === id)
     // Create a new object from the original note and overwrite the 
     // 'important' property by assigning it again.
     const changedNote = { ...note, important: !note.important }
 
-    // HTTP PUT replaces an entire note with a new note.
-    axios
-      .put(url, changedNote)
-      .then(response => {
-        // Create a new array by mapping every item from the old array into an item in the new array. If note.id !== id is true, we copy the item from the old array into the new array. If false, then the note object returned by the server is added to the array instead.
-        setNotes(notes.map(n => n.id !== id ? n : response.data))
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+        console.debug(`Importance of ${id} is toggled`)
       })
-    console.debug(`Importance of ${id} is toggled`)
-  }
-
-
-  // FUNC:  LOAD INITIAL NOTES FROM db.json FILE ON SERVER
-  // The hook is executed as an effect with useEffect
-  const hook = () => {
-    // HTTP GET NOTES FROM db.json
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.debug("Axios response from loading initial notes:", response)
-        setNotes(response.data)
-      })
-  }
-  useEffect(hook, []) // Effects occur once at the end of rendering
+    }
 
 
   // FUNC:  ADD NOTE ON CLICK 'save' BUTTON
@@ -50,16 +45,21 @@ const App = (props) => {
 
     const noteObject = {
       content: newNote,
-      important: Math.random() < 0.5,
-      id: notes.length + 1,
+      important: Math.random() < 0.5
+      // id: notes.length + 1,
     }
 
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
     // HTTP POST NEW NOTE INTO db.json
     axios
       .post('http://localhost:3001/notes', noteObject)
-      .then(response => {
-        console.debug("Axios response from adding note:", response)
-        setNotes(notes.concat(noteObject))
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
         setNewNote('')
       })
   }
